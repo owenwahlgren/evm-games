284e7a46c30b49a66c604172f822e2affc796e4e
owenwahlgren <owenwahlgren@Owens-MBP.cable.rcn.com>
2022-07-20 19:50:51 -0400

    update footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ import styles from '../../styles/Home.module.css';
 export default function Footer() {
     return(
         <footer className={styles.footer}>
-        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/owenwahlgren" target="_blank" rel="noopener noreferrer">
           <i className="bi bi-twitter"></i>
         </a>
         <a href="https://discord.gg" target="_blank" rel="noopener noreferrer">
           <i className="bi bi-discord"></i>
         </a>
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/owenwahlgren/evm-games" target="_blank" rel="noopener noreferrer">
           <i className="bi bi-github"></i>
         </a>
         <a href="/about" target="_blank" rel="noopener noreferrer">
